f207e2dc3879d09e86cd16f77fd389e7de5e2bd0
sergio.junior <sergio.junior@mac159102.local>
2012-04-12 20:45:10 -0300

    Adicionado 'esqueleto' do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eter::Application.config.secret_token = '704ce5ddc4bdd9f2d64df5fe90deb1f806aa4915f829fba1477cdbe136056fbcc728ff39a2c16922a66d325f1c035faeefe223563c399bb0b9b17e30e4ef0e10'
