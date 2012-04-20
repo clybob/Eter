910fed937bd621d38476976f902b355da8f22ff0
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-04-19 21:01:11 -0300

    Alterado estilo de código do model user, para usar dois espaçamentos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-     validates :name, :presence => true 
-     validates :email, :presence => true
+  validates :name, :presence => true 
+  validates :email, :presence => true
 end
