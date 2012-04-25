1242e6bbfebc576b25518abbfa04e3599f40e346
sergio.junior <sergio.junior@mac159102.local>
2012-04-25 02:58:54 -0300

    Add test to coverage search in model user

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be search a user" do
+    user = User.search('Zacarias', 1)
+    assert_equal user[0].name, 'Zacarias'
+  end
+
+  test "should be search a blank user and return all users" do
+    user = User.search('', 1)
+    assert_equal user.count, User.all.count
+  end
 end
