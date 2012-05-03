b01177611176c9b0c1940e52ae8d92f664f77f2e
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-05-03 20:52:38 -0300

    Adicionada asserção de exbição  dos dados na index

--- test/functional/editorials_controller_test.rb
+++ test/functional/editorials_controller_test.rb
@@ -9,6 +9,7 @@ class EditorialsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:editorials)
+    assert_index_tags 2, 4
   end
 
   test "should get new" do
