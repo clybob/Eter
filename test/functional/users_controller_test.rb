22872074681ae8a8b6704caf7be041e0803cde59
zacarias <zacarias@corp.globo.com>
2012-05-03 19:41:05 -0300

    Teste de index

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -9,6 +9,13 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
+    assert_tag :tag => "table", :attributes => {:class=>"table table-striped"},
+                   :child => {:tag => 'tbody',
+                       :children => {:count => 10, :only=>{:tag=>"tr"}},
+                       :child => {:tag => 'tr',
+                           :children => {:count => 7, :only=>{:tag=>"td"}},
+                       }
+                  }
   end
 
   test "should get new" do
