a1c7dcc5ba26ac38fec7b2b25198ee5dd8f41992
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-05-03 20:49:33 -0300

    Criado helper para testar a exibição dos dados na index, e verificar a existencia do paginate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def assert_index_tags( tr_count, td_count )
+    assert_tag :tag => 'table', :attributes => {:class=>'table table-striped'},
+               :child => {:tag => 'tbody',
+               :children => {:count => tr_count, :only=>{:tag=>'tr'}},
+                 :child => {:tag => 'tr',
+                   :children => {:count => td_count, :only=>{:tag=>'td'}}
+                 }
+               }
+  end
+
+  def assert_paginate_is_displayed
+    assert_tag :tag => 'div', :attributes => {:class => 'pagination'}
+  end
 end
+
